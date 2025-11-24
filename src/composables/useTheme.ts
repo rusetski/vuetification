766b5840a0a5242bac27fe5085a2e18a5c52af39
Evgen <benichester@gmail.com>
2025-11-24 20:10:49 +0100

    feat: add SSR condition to useTheme composable

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -13,8 +13,10 @@ const defaultTheme: ThemeOptions = {
 export const installTheme = (options?: ThemeOptions) => {
   const colors = Object.assign({}, defaultTheme.colors, options?.colors);
 
-  Object.entries(colors).forEach(([key, value]) => {
-    const root = document.querySelector(':root') as HTMLElement;
-    root.style.setProperty(`--theme-color-${key}`, value);
-  });
+  if (!import.meta.env.SSR) {
+    Object.entries(colors).forEach(([key, value]) => {
+      const root = document.querySelector(':root') as HTMLElement;
+      root.style.setProperty(`--theme-color-${key}`, value);
+    });
+  }
 };
