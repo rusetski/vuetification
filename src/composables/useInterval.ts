591d560c11e4d34512a7532d2571ff9f6fcf62da
Evgen <benichester@gmail.com>
2025-11-26 16:28:18 +0100

    feat: add repeat and immediately options to interval composable

--- src/composables/useInterval.ts
+++ src/composables/useInterval.ts
@@ -1,18 +1,46 @@
-import { onBeforeUnmount } from 'vue';
+import { ref, onBeforeUnmount } from 'vue';
+
+type Callback = (iteration: number) => void;
+
+type OptionsOrCallback = Callback | Options;
 
 interface Options {
+  callback: Callback;
+  delay: number;
   stopOnUnmount?: boolean;
+  repeat?: number | undefined;
+  immediately?: boolean;
 }
 
-const defaultOptions = {
-  stopOnUnmount: true
+const defaultOptions: Partial<Options> = {
+  delay: 1000,
+  stopOnUnmount: true,
+  repeat: undefined,
+  immediately: false
 };
 
-export const useInterval = ({ stopOnUnmount }: Options = defaultOptions) => {
+export const useInterval = (optionsOrCallback: OptionsOrCallback, intervalDelay?: number) => {
+  const isFirstArgFunction = typeof optionsOrCallback === 'function';
+  const callback = isFirstArgFunction ? optionsOrCallback : optionsOrCallback?.callback;
+  const delay = isFirstArgFunction ? intervalDelay : optionsOrCallback?.delay;
+  const {
+    stopOnUnmount = defaultOptions.stopOnUnmount,
+    repeat = defaultOptions.repeat,
+    immediately = defaultOptions.immediately
+  } = isFirstArgFunction ? defaultOptions : optionsOrCallback || defaultOptions;
   let interval: ReturnType<typeof setInterval>;
+  const iteration = ref(0);
 
   const stop = () => {
     clearInterval(interval);
+  };
+
+  const hundler = () => {
+    if (callback) callback(iteration.value);
+
+    iteration.value++;
+
+    if (typeof repeat === 'number' && repeat <= iteration.value) stop();
   };
 
   onBeforeUnmount(() => {
@@ -22,9 +50,14 @@ export const useInterval = ({ stopOnUnmount }: Options = defaultOptions) => {
   });
 
   return {
-    start(callback: () => void, time: number) {
+    iteration,
+    start() {
       if (interval) stop();
-      interval = setInterval(callback, time);
+
+      iteration.value = 0;
+
+      if (immediately) hundler();
+      interval = setInterval(hundler, delay || defaultOptions.delay);
     },
     stop
   };
